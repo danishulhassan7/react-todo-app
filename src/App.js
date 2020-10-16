2684182840fb27ec99b873e72f47a703ad598804
Danish Ul Hassan <danishulhassan7>
2020-10-16 09:25:12 +0500

    Done with the Add Task Functionality

--- src/App.js
+++ src/App.js
@@ -3,10 +3,45 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 class App extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+      todos: ["Virat","Smith","Baber","Williamson","Rohit"],
+      value: ''
+    };
+  }
+
+  // add_todo = () => {
+  //   this.state.todos.push(this.state.value);
+  //   this.setState({
+  //     todos: this.state.todos
+  //   })
+  // }
+
+  // 2nd Method to add_todo 
+
+  add_todo = () => {
+    this.setState({
+      todos: [...this.state.todos, this.state.value],
+      value: ''
+    })
+  }
   render() {
+    let {todos,value} = this.state;
     return(
       <div>
-        <h2>Initial Setup</h2>
+        <input value={value} onChange={(e) => this.setState({value: e.target.value})} type="text" placeholder="Enter any task"/>
+        <button onClick={this.add_todo}>Add Task</button>
+        <ul>
+          {
+            todos.map((v,i)=>{
+            return (
+              <li key={i}> {v} </li>
+            );
+            })
+          }
+        </ul>
       </div>
     );
   }
